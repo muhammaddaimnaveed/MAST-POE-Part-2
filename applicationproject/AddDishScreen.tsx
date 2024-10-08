dfc1d45de993f01b59ba0d275676cc90c2789669
Muhammad Naveed <mdaim1101@gmail.com>
2024-10-08 12:11:06 +0200

    /*This Screen allows the chef to add the new dishes name, price, description and image which users can view in the Menu Screen */

--- applicationproject/AddDishScreen.tsx
+++ applicationproject/AddDishScreen.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { StyleSheet, View, Text, Button, Image, TouchableOpacity, Alert, TextInput } from 'react-native';
 
+/*This Screen allows the chef to add the new dishes name, price, description and image which users can view in the Menu Screen */
+
 const AddDishScreen1: React.FC<{ navigation: any, route: any }> = ({ navigation, route }) => {
   const [dishName, setDishName] = useState<string>('');
   const [price, setPrice] = useState<string>('');
